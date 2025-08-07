0cc8d4cf21b21be8ea601220e52a9189df008ed0
GillyZ2000 <tigergillyy@gmail.com>
2025-08-07 13:24:31 +0300

    ad matai?

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,1 +1,27 @@
 
+#include <unistd.h>
+
+struct MallocMetadata {
+    size_t size;
+    bool is_free;      MallocMetadata* next;
+    MallocMetadata* prev;
+};
+
+MallocMetadata* front = nullptr;
+MallocMetadata* back = nullptr;
+
+size_t _num_free_blocks(){
+    size_t count = 0;
+    MallocMetadata* curr = front;
+    while(curr){
+        if(curr->is_free)
+        {
+            count++;
+        }
+    }
+    return count;
+ }
+
+size_t _num_free_bytes();
+
+
